7efbad94df409cceb147ab24d16352524ba2a38c
makun <michaelmakun@aliyun.com>
2017-03-13 19:42:26 +0800

    add flash to project

---
+++ app/helpers/flashes_helper.rb
@@ -0,0 +1,11 @@
+module  FlashesHelper
+  FLASHES_CLASSES = { notice: "success", warning: "warning", alert: "danger"}.freeze
+
+  def flash_class(key)
+    FLASHES_CLASSES.fetch key.to_sym, key
+  end
+
+  def user_facing_flashes
+    flash.to_hash.slice "notice","warning","alert"
+  end
+end
