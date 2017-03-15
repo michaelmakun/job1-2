dfd79e105e5509d18af3ff12e0349c52d064d171
makun <michaelmakun@aliyun.com>
2017-03-15 18:07:58 +0800

    change size of fa-lg in resume/index

--- app/helpers/admin/jobs_helper.rb
+++ app/helpers/admin/jobs_helper.rb
@@ -2,9 +2,9 @@ module Admin::JobsHelper
 
   def render_job_status(job)
     if job.is_hidden
-      content_tag(:span, "", :class => "fa fa-lock")
+      content_tag(:span, "", :class => "fa fa-lock fa-lg")
     else
-      content_tag(:span, "", :class => "fa fa-eye")
+      content_tag(:span, "", :class => "fa fa-eye fa-lg")
     end
   end
 end
