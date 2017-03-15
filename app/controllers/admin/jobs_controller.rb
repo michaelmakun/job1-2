21116c2ae49701857c0884628cd5045c57b93764
makun <michaelmakun@aliyun.com>
2017-03-15 21:59:34 +0800

    add order in admin#index

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -4,7 +4,16 @@ class Admin::JobsController < ApplicationController
   layout 'admin'
 
   def index
-    @jobs = Job.all
+    @jobs = case params[:order]
+    when 'job_resumes_count'
+      Job.all.sort_by {|job| job.resumes.count }.reverse
+    when 'by_lower_bound'
+      Job.published.order('wage_lower_bound DESC')
+    when 'by_upper_bound'
+      Job.published.order('wage_upper_bound DESC')
+    else
+      Job.published.recent
+    end
   end
 
   def show
